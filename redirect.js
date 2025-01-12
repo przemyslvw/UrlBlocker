ae69196b9016e304a8f33a74e085347cf6bdc2aa
Przemysław Ubuntu <przemyslvw@gmail.com>
2025-01-12 14:45:54 +0100

    przekierowanie w przypadku wejścia bezpośredniego na url

---
+++ redirect.js
@@ -0,0 +1,12 @@
+chrome.storage.sync.get("urls", (data) => {
+    const urls = data.urls || [];
+    const currentUrl = window.location.href;
+
+    // Sprawdź, czy bieżący URL znajduje się na liście
+    if (urls.some((url) => currentUrl.includes(url))) {
+        console.log(`[URL Manager] Redirecting: ${currentUrl} -> replacement.html`);
+
+        // Przekierowanie do lokalnego pliku HTML
+        window.location.href = chrome.runtime.getURL("replacement.html");
+    }
+});
